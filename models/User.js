7ce44d3ba119c8734f61931752109b12356ba364
GraceCarchidi <s1253453@monmouth.edu>
2021-04-08 11:40:20 -0400

    installed bcrypt and added code to utilize bcrypt

--- models/User.js
+++ models/User.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
-    const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
 
-const UserSchema = new Schema(
-    {
-        username: String,
-        password: String
-    }
-);
+const UserSchema = new Schema({
+    username: String,
+     password: String
+});
+
+UserSchema.pre('save', function(next){ 
+    const user = this 
+
+
+    bcrypt.hash(user.password, 10, (error, hash) => { 
+        user.password = hash 
+        next() 
+    })
+})
+
 
 const User = mongoose.model('User', UserSchema);
 
