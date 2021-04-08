7009ac705bc6ac36caea7c3d21a4008a5ff9a6c2
GraceCarchidi <s1253453@monmouth.edu>
2021-04-08 11:54:20 -0400

    added code to redirect due to errors in registration

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -2,9 +2,10 @@ const User = require('../models/User');
 const path = require('path');
 
 module.exports = (req, res) => {
-    User.create(req.body,
-        (error, user) => {
+    User.create(req.body,(error, user) => {
+            if(error){
+                return res.redirect('/auth/register')
+            }
             res.redirect('/');
-        }
-    );
+        })
 }
