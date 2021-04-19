f287186286db582ca0d1792a1e25e7bfda7ee867
GraceCarchidi <s1253453@monmouth.edu>
2021-04-19 16:04:27 -0400

    installed connect flash to flush messages and added reference in index.js and New and storeUser.js

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -6,7 +6,8 @@ module.exports = (req, res) => {
         (error, user) => {
             if(error) {
                 const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
-                req.session.validationErrors = validationErrors
+                req.flash('validationErrors',validationErrors)
+                //req.session.validationErrors = validationErrors
                 return res.redirect('/auth/register');
             }
 
