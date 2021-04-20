d5203d8a834cabf9ca000518d30b95e3905f26ec
B-T-Johnson <s1218241@monmouth.edu>
2021-04-20 15:10:46 -0400

    Added custom error messages in 'loginUser' controller & refactored 'login' controller/view to persist request data

--- controllers/login.js
+++ controllers/login.js
@@ -1,3 +1,15 @@
-module.exports = (req, res) =>{
-    res.render('login');
+module.exports = (req, res) => {
+    var username = "";
+    const data = req.flash('data')[0];
+
+    if(typeof data != "undefined") {
+        username = data.username;
+    }
+
+    res.render('login',
+        {
+            errors: req.flash('validationErrors'),
+            username: username
+        }
+    );
 }
