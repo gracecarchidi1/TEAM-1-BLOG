03abf08cfc9cd915b6716d49becb5e3d68bbfa5f
B-T-Johnson <s1218241@monmouth.edu>
2021-04-20 15:13:20 -0400

    Made password field persistent on login form

--- controllers/login.js
+++ controllers/login.js
@@ -1,15 +1,17 @@
 module.exports = (req, res) => {
-    var username = "";
+    var username = "", password = "";
     const data = req.flash('data')[0];
 
     if(typeof data != "undefined") {
         username = data.username;
+        password = data.password;
     }
 
     res.render('login',
         {
             errors: req.flash('validationErrors'),
-            username: username
+            username: username,
+            password: password
         }
     );
 }
